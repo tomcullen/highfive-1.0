4ac275aa8fc0017848b440523115bf4bc2db7b16
Thomas Cullen <cullentj@yahoo.com>
2012-01-06 14:06:28 -0600

    fixed links to company from jobs index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-    @current_user ||= User.find(session[:user_id])
+    @current_user ||= User.find_by_id(session[:user_id])
   end
 
   def logged_in?
