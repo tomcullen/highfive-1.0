83f88bd1ed9713e3508d80f3e929f26709719dbf
Thomas Cullen <cullentj@yahoo.com>
2012-02-23 12:02:45 -0600

    Configured for redistogo

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,3 +1,6 @@
+uri = URI.parse(ENV["REDISTOGO_URL"])
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 Dir[File.join(Rails.root, 'app', 'jobs', '*.rb')].each { |file| require file }
 config = YAML::load(File.open("#{Rails.root}/config/redis.yml"))[Rails.env]
-Resque.redis = Redis.new(:host => config['host'], :port => config['port'])
+# Resque.redis = Redis.new(:host => config['host'], :port => config['port'])
+Resque.redis = REDIS
